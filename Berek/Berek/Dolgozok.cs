57a41f3f6e831dded0e12c0e8e6829a3c04b1211
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2024-11-19 10:41:42 +0100

    Add files via upload

---
+++ Berek/Berek/Dolgozok.cs
@@ -0,0 +1,27 @@
+﻿namespace Berek;
+
+internal class Dolgozok
+{
+    public string Nev { get; set; }
+    public string Neme { get; set; }
+    public string Reszleg { get; set; }
+    public int MunkaViszonyKezdete { get; set; }
+    public int Ber { get; set; }
+
+
+
+    public Dolgozok(string sor) 
+    {
+    var v = sor.Split(";");
+        Nev = v[0];
+        Neme = v[1];
+        Reszleg = v[2];
+        MunkaViszonyKezdete = int.Parse(v[3]);
+        Ber = int.Parse(v[4]);
+    }
+
+    public override string ToString()
+    {
+        return $"{Nev}, {Neme}, {Reszleg}, {MunkaViszonyKezdete}, {Ber}";
+    }
+}
---
+++ Berek/Berek/Program.cs
@@ -0,0 +1,44 @@
+﻿using Berek;
+
+var dolgozok = File.ReadAllLines(@"../../../src/berek2020.txt").Skip(1).Select(line => new Dolgozok(line)).ToList();
+
+Console.WriteLine("3. feladat");
+Console.WriteLine($"Az itt dolgozok adatainak száma {dolgozok.Count}");
+Console.WriteLine("4.feladat");
+var avgber2020 = dolgozok.Average(x => x.Ber);
+Console.WriteLine($"átlag bérek 2020ban {avgber2020}");
+Console.WriteLine("5.feladat");
+Console.Write("Kérem adja meg a részleg nevét: ");
+string keresettReszleg = Console.ReadLine();
+
+var keresettReszlegDolgozok = dolgozok.Where(d => d.Reszleg.Equals(keresettReszleg, StringComparison.OrdinalIgnoreCase)).ToList();
+
+if (keresettReszlegDolgozok.Any())
+{
+    Console.WriteLine($"A {keresettReszleg} részlegen dolgozók:");
+    foreach (var dolgozo in keresettReszlegDolgozok)
+    {
+        Console.WriteLine(dolgozo);
+    }
+
+
+    var legnagyobbBerDolgozo = keresettReszlegDolgozok.OrderByDescending(d => d.Ber).First();
+    Console.WriteLine($"A legnagyobb bérrel rendelkező dolgozó: {legnagyobbBerDolgozo}");
+}
+else
+{
+    Console.WriteLine($"A megadott részleg nem létezik a cégnél!");
+}
+
+Console.WriteLine("7. feladat");
+var reszlegekStat = dolgozok
+    .GroupBy(d => d.Reszleg)
+    .Select(group => new { Reszleg = group.Key, DolgozokSzama = group.Count() })
+    .OrderBy(stat => stat.Reszleg);  
+
+Console.WriteLine("Részlegek statisztikája:");
+foreach (var stat in reszlegekStat)
+{
+    Console.WriteLine($"{stat.Reszleg}: {stat.DolgozokSzama} fő");
+}
+// elkészítési idő 47 perc
